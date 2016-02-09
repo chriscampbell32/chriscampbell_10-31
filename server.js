796aabcd8d9093a7fa578029d3412d80b39bd47d
chriscampbell32 <chriscampbell32@yahoo.com>
2016-02-09 12:53:25 -0500

    got server working, local host is good

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var PORT = process.env.PORT || 8090;
+
+
+// app.use(express.static("public"));
+// app.use(express.static())
+
+app.get('/', function(req,res){
+    res.sendFile(process.cwd() + '/views/homepage.html');
+});
+
+app.get('/about', function(req, res){
+    res.sendFile(process.cwd() + '/views/about.html');
+});
+
+app.get('/works', function(req, res){
+    res.sendFile(process.cwd() + '/views/works.html');
+});
+
+app.get('/sayhi', function(req, res){
+    res.sendFile(process.cwd() + '/views/sayhi.html');
+});
+
+app.listen(PORT, function(req,res){
+    console.log('server is listening on port %s', PORT)
+});
