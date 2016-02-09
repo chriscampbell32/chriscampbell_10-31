94d966db9e6826556b78cdf7dfa53961121b33d3
chriscampbell32 <chriscampbell32@yahoo.com>
2016-02-09 16:47:53 -0500

    express-session work

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var app = express();
 var PORT = process.env.PORT || 8090;
+
+
+
+//middleware 
+
+function myLoggingMiddleware(req, res, next){
+    var url = request.url;
+    var method = request.method;
+
+    console.log("%s request at %s", url, method);
+    next();
+}
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(myLoggingMiddleware);
+app.use(session({
+    secret: 'my super secret',
+    cookie: {maxAge: 60000},
+    saveUninitialiazed: true,
+    resave: false
+}));
 
 
 app.use(express.static("public"));
